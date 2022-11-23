77b51171d6323679e9383e918d9caae01b2c1f38
oneminch <oneminch@users.noreply.github.com>
2022-11-23 21:25:05 +0000

    deploy: 4dbf0db90322e87254048df2a88c15ce52d3831f

---
+++ _nuxt/static/1669238704/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fxlist\u002F_nuxt\u002Fstatic\u002F1669238704",layout:"default",error:a,state:{lists:{},listsPopulated:false},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fxlist\u002F",assetsPath:"\u002Fxlist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
